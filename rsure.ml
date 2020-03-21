aa25199de3124d8996602ffb0957494590bab75f
David Brown <davidb@davidb.org>
2020-03-21 16:19:31 -0600

    Add another little benchmark for gzip in
    This benchmark reads a fixed existing file.

--- rsure.ml
+++ rsure.ml
@@ -5,4 +5,15 @@ let nothing () =
   Sequence.iter (Walk.walk ".") ~f:(fun e ->
     print_endline @@ Node.show e)
 
-let _ = Weave.Stream.bulk_io ()
+let bench_stream () = Weave.Stream.bulk_io ()
+
+(* Read a large file to get an idea of timing. *)
+let bench_gzip_in () =
+  let fd = Weave.Stream.gzip_in "/home/2sure.dat.gz" in
+  let rec loop count =
+    match fd#read_line with
+      | Some _ -> loop (count + 1)
+      | None -> count in
+  let total = loop 0 in
+  fd#close;
+  printf "%d lines\n" total
